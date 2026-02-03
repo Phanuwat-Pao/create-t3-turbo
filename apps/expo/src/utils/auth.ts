28dad9fd50d7200bbfce0ebca8b1f2977104b554
Phanuwat Luadthai <phanuwat.lua@gmail.com>
2026-02-03 16:11:45 +0700

    chore: Remove Expo better-auth client configuration.

--- apps/expo/src/utils/auth.ts
+++ apps/expo/src/utils/auth.ts
@@ -1,5 +1,21 @@
 import { expoClient } from "@better-auth/expo/client";
+import { oauthProviderClient } from "@better-auth/oauth-provider/client";
+import { passkeyClient } from "@better-auth/passkey/client";
+import {
+  adminClient,
+  organizationClient,
+  multiSessionClient,
+  deviceAuthorizationClient,
+  lastLoginMethodClient,
+  magicLinkClient,
+  emailOTPClient,
+  usernameClient,
+  phoneNumberClient,
+  oneTimeTokenClient,
+  genericOAuthClient,
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import Constants from "expo-constants";
 import * as SecureStore from "expo-secure-store";
 
 import { getBaseUrl } from "./base-url";
@@ -8,9 +24,33 @@ export const authClient = createAuthClient({
   baseURL: getBaseUrl(),
   plugins: [
     expoClient({
-      scheme: "expo",
+      scheme: Constants.expoConfig?.scheme as string,
       storage: SecureStore,
-      storagePrefix: "expo",
+      storagePrefix: Constants.expoConfig?.scheme as string,
     }),
+    organizationClient(),
+    // twoFactorClient({
+    //   onTwoFactorRedirect() {
+    //     window.location.href = "/two-factor";
+    //   },
+    // }),
+    adminClient(),
+    multiSessionClient(),
+    // oneTapClient({
+    //   clientId: env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    //   promptOptions: {
+    //     maxAttempts: 1,
+    //   },
+    // }),
+    deviceAuthorizationClient(),
+    lastLoginMethodClient(),
+    passkeyClient(),
+    oauthProviderClient(),
+    magicLinkClient(),
+    emailOTPClient(),
+    usernameClient(),
+    phoneNumberClient(),
+    oneTimeTokenClient(),
+    genericOAuthClient(),
   ],
 });
